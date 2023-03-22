5d77950cd79e7d29163153c14af140009458e2a6
rashomong <jiwoojjang77@gmail.com>
2023-03-22 20:31:11 +0900

    처음 추가

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480+300+100")    # 크기 설정 알파뱃 'x'사용 + 열리는 x,y 좌표 위치
+root.resizable(False, False)    # 창 크기 x, y 조절이 불가능. 최대화 불가능
+
+
+root.mainloop() #꺼지지 않도록 루프
+
+
---
+++ gui_basic/2_button.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+
+btn1 = Button(root, text="버튼1")   # 버튼 생성
+btn1.pack()     # 버튼을 프로그램에 포함시킴
+
+btn2 = Button(root, padx=5, pady=10, text="버튼2")  #버튼 글자 기준으로 공간 확보
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5, text="버튼3")
+btn3.pack()
+
+btn4 = Button(root, width=10, height=5, text="버튼4")   # 버튼 크기 설정 (택스트가 길면 짤림)
+btn4.pack()
+
+btn5 = Button(root, fg="red", bg="yellow", text="버튼5")
+btn5.pack()
+
+photo = PhotoImage(file="gui_basic\image.png")
+btn6 = Button(root, image=photo) 
+btn6.pack()
+
+root.mainloop() #꺼지지 않도록 루프
+
+
+
+
